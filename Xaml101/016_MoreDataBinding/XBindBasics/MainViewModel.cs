7223d0818b16fb7cd7ae30f53c642704b5f9c39f
Bill Reiss <bill.reiss@outlook.com>
2016-03-28 09:01:06 -0400

    Part 16 more data binding

---
+++ Xaml101/016_MoreDataBinding/XBindBasics/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace XBindBasics
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public string FieldBindingText = "Here is a Field Binding";
+
+        public MainViewModel()
+        {
+            this.PropertyBindingText = "This is a Property Binding";
+        }
+
+        string propertyBindingText;
+        public string PropertyBindingText
+        {
+            get
+            {
+                return propertyBindingText;
+            }
+            set
+            {
+                propertyBindingText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public void ClickHandler()
+        {
+            PropertyBindingText = "Clicked!";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        } 
+
+    }
+}
