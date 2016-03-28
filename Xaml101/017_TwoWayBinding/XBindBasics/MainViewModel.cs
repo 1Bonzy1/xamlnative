6b6a5fe40e909b8dfb52c6a367dda9db55182b07
Bill Reiss <bill.reiss@outlook.com>
2016-03-28 09:05:46 -0400

    Part 17 Two Way Binding

---
+++ Xaml101/017_TwoWayBinding/XBindBasics/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace XBindBasics
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        bool? checkBoxValue = false;
+        string textBoxValue;
+
+        public bool? CheckBoxValue
+        {
+            get
+            {
+                return checkBoxValue;
+            }
+
+            set
+            {
+                checkBoxValue = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string TextBoxValue
+        {
+            get
+            {
+                return textBoxValue;
+            }
+
+            set
+            {
+                textBoxValue = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+        }
+
+
+        public async void ClickHandler(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Windows.UI.Popups.MessageDialog(string.Format("CheckBox:{0}, TextBox:{1}", CheckBoxValue, TextBoxValue));
+            await dlg.ShowAsync();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        } 
+
+    }
+}
