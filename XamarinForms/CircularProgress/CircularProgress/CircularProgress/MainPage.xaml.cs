fae7a0e58b40ef2cca9879676286846516526b50
Bill Reiss <bill.reiss@outlook.com>
2016-04-14 17:09:45 -0500

    Circular Progress control Xamarin Forms sample

---
+++ XamarinForms/CircularProgress/CircularProgress/CircularProgress/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CircularProgress
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.Device.StartTimer(TimeSpan.FromSeconds(.02), OnTimer);
+        }
+
+        private bool OnTimer()
+        {
+            var progress = (progressControl.Progress + .01) ;
+            if (progress > 1) progress = 0;
+            progressControl.Progress = progress;
+            return true;
+        }
+    }
+}
