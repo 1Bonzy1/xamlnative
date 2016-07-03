800e641eb8aa99e217c031bf65a8fa8f3ababf90
Bill Reiss <bill.reiss@outlook.com>
2016-07-03 14:57:52 -0400

    Added sample for Xamarin Forms ListView without Data Binding

---
+++ XamarinForms/ListViewNoBinding/ListViewNoBinding/ListViewNoBinding/ItemCell.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ListViewNoBinding
+{
+    public partial class ItemCell : ViewCell
+    {
+        public ItemCell()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            var itm = BindingContext as ItemViewModel;
+            if (itm != null)
+            {
+                idLabel.Text = itm.Id.ToString();
+                textLabel.Text = itm.Text.ToString();
+            }
+            else
+            {
+                idLabel.Text = textLabel.Text = "";
+            }
+        }
+    }
+}
---
+++ XamarinForms/ListViewNoBinding/ListViewNoBinding/ListViewNoBinding/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewNoBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            listView.ItemsSource = GetItems();
+        }
+
+        public List<ItemViewModel> GetItems()
+        {
+            var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum".Split(' ');
+            var items = new List<ItemViewModel>();
+            for (int i = 0; i < 500; i++)
+            {
+                items.Add(new ItemViewModel() { Id = i, Text = lorem[i % lorem.Length] });
+            }
+            return items;
+        }
+    }
+}
